338bcc21daecd078e1d9d9ee0bfbef93f9967acc
TomsNikelis <nikelistoms@gmail.com>
2021-03-19 09:33:38 +0200

    Skanas pievienosana un to funkciju ieviesana

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Audio : MonoBehaviour
+{
+    public AudioSource voiceLine1Source;
+    public AudioSource voiceLine2Source;
+    public AudioClip voiceLine1Clip;
+    public AudioClip voiceLine2Clip;
+    public void HangleInputData(int value)
+    {
+        if(value == 0)
+        {
+            voiceLine1Source.PlayOneShot(voiceLine1Clip);
+            Debug.Log(value);
+        }
+        if (value == 1)
+        {
+            voiceLine2Source.PlayOneShot(voiceLine2Clip);
+
+        }
+    }
+}
---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioSource buttonAudio;
+    public AudioClip clickClip;
+    public void ClickSound()
+    {
+        buttonAudio.PlayOneShot(clickClip);
+    }
+}
